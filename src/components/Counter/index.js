0540760990d4965a5e75049b756ce114fdd9b1bd
Marcos Teodoro <marcosvinici_us@live.com>
2020-02-25 16:24:53 -0300

    Carrega o tempo que falta para meu aniverśario

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -5,23 +5,45 @@ import './style.css'
 import CounterUnit from '../CounterUnit'
 
 class Counter extends React.Component {
+  constructor() {
+    super()
+
+    const birthday = moment([2020, 2, 7])
+
+    // TODO: REVIEW
+    const days = birthday.diff(moment.now(), 'days')
+    const hours = birthday.diff(moment.now(), 'hours') - (24 * days) 
+    const minutes =  birthday.diff(moment.now(), 'minutes') - (days * 24 * 60) - (hours * 60)
+    const seconds = birthday.diff(moment.now(), 'seconds') - (days * 24 * 60 * 60) - (hours * 60 * 60) - (minutes * 60)
+    // TODO: REVIEW
+
+    this.state = {
+      days,      
+      hours,      
+      minutes,      
+      seconds
+    }
+
+    console.log(this.state);
+  }
+
   render() {
     return (
       <>
         <div className="counter-box">
-          <CounterUnit unitName="days" unitValue="10" />
+          <CounterUnit unitName="days" unitValue={ this.state.days } />
         </div>
 
         <div className="counter-box">
-          <CounterUnit unitName="hours" unitValue="09" />
+          <CounterUnit unitName="hours" unitValue={ this.state.hours } />
         </div>
 
         <div className="counter-box">
-          <CounterUnit unitName="minutes" unitValue="08" />
+          <CounterUnit unitName="minutes" unitValue={ this.state.minutes } />
         </div>
 
         <div className="counter-box">
-          <CounterUnit unitName="seconds" unitValue="07" />
+          <CounterUnit unitName="seconds" unitValue={ this.state.seconds } />
         </div>
       </>
     )
