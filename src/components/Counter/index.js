6f0b689701b0095517407722ef4ffe923240fd78
Marcos Teodoro <marcosvinici_us@live.com>
2020-02-25 16:33:41 -0300

    Adiciona time interval para atualizar contador

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -8,42 +8,52 @@ class Counter extends React.Component {
   constructor() {
     super()
 
-    const birthday = moment([2020, 2, 7])
-
-    // TODO: REVIEW
-    const days = birthday.diff(moment.now(), 'days')
-    const hours = birthday.diff(moment.now(), 'hours') - (24 * days) 
-    const minutes =  birthday.diff(moment.now(), 'minutes') - (days * 24 * 60) - (hours * 60)
-    const seconds = birthday.diff(moment.now(), 'seconds') - (days * 24 * 60 * 60) - (hours * 60 * 60) - (minutes * 60)
-    // TODO: REVIEW
-
     this.state = {
-      days,      
-      hours,      
-      minutes,      
-      seconds
+      days: '',
+      hours: '',
+      minutes: '',
+      seconds: ''
     }
+  }
 
-    console.log(this.state);
+  componentDidMount() {
+    setInterval(() => {
+      const birthday = moment([2020, 2, 7])
+
+      // TODO: REVIEW
+      const days = birthday.diff(moment.now(), 'days')
+      const hours = birthday.diff(moment.now(), 'hours') - (24 * days)
+      const minutes = birthday.diff(moment.now(), 'minutes') - (days * 24 * 60) - (hours * 60)
+      const seconds = birthday.diff(moment.now(), 'seconds') - (days * 24 * 60 * 60) - (hours * 60 * 60) - (minutes * 60)
+      // TODO: REVIEW
+
+      this.setState({
+        days,
+        hours,
+        minutes,
+        seconds
+      });
+
+    }, 1000);
   }
 
   render() {
     return (
       <>
         <div className="counter-box">
-          <CounterUnit unitName="days" unitValue={ this.state.days } />
+          <CounterUnit unitName="days" unitValue={this.state.days} />
         </div>
 
         <div className="counter-box">
-          <CounterUnit unitName="hours" unitValue={ this.state.hours } />
+          <CounterUnit unitName="hours" unitValue={this.state.hours} />
         </div>
 
         <div className="counter-box">
-          <CounterUnit unitName="minutes" unitValue={ this.state.minutes } />
+          <CounterUnit unitName="minutes" unitValue={this.state.minutes} />
         </div>
 
         <div className="counter-box">
-          <CounterUnit unitName="seconds" unitValue={ this.state.seconds } />
+          <CounterUnit unitName="seconds" unitValue={this.state.seconds} />
         </div>
       </>
     )
