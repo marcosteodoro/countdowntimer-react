d787bd209e336c1fd39536dd2cf2be15406bdede
Marcos Teodoro <marcosvinici_us@live.com>
2020-02-26 11:40:11 -0300

    Adiciona esqueleto do component CounterConfigurator

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react'
 import './App.css'
 
 import Counter from './components/Counter'
+import CounterConfigurator from './components/CounterConfigurator'
 
 export default function App() {
   return (
     <div className="App">
+      <CounterConfigurator />
       <Counter />
     </div>
   )
---
+++ src/components/CounterConfigurator/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class ConterConfigurator extends React.Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <></>
+    )
+  }
+}
