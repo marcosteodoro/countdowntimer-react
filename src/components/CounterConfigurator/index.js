a55b39b35212e8472fff427fcc64fc1f242fc2d7
Marcos Teodoro <marcosvinici_us@live.com>
2020-02-27 11:55:00 -0300

    Adiciona form de configuração do contador

--- src/components/CounterConfigurator/index.js
+++ src/components/CounterConfigurator/index.js
@@ -1,13 +1,36 @@
 import React from 'react'
+import './style.css'
 
 export default class ConterConfigurator extends React.Component {
   constructor() {
     super()
   }
 
+  handleSubmit(ev) {
+    ev.preventDefault()
+  }
+
   render() {
     return (
-      <></>
+      <form className="counter-configurator" onSubmit={ this.handleSubmit }>
+        <div className="counter-configurator-field">
+          <label htmlFor="date">
+            Data: 
+            <input type="date" id="date"/>
+          </label>
+        </div>
+        <div className="counter-configurator-field">
+          <label htmlFor="time">
+            Hora: 
+            <input type="time" id="time"/>
+          </label>
+        </div>
+        <div className="counter-configurator-field">
+          <button>
+            Iniciar
+          </button>
+        </div>
+      </form>
     )
   }
 }
