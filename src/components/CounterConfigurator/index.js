8bd37b60f63d7f2388b6411a68d4d8b6cd8a26b0
Marcos Teodoro <marcosvinici_us@live.com>
2020-02-28 11:44:44 -0300

    Monta estados iniciais dos inputs de data e hora

--- src/components/CounterConfigurator/index.js
+++ src/components/CounterConfigurator/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import moment from 'moment'
 import './style.css'
 
 export default class ConterConfigurator extends React.Component {
   constructor() {
     super()
+
+    this.state = {
+      'date': moment().format('YYYY-MM-DD'),
+      'time': moment().add('30', 'minutes').format('h:mm')
+    }
   }
 
   handleSubmit(ev) {
@@ -16,13 +22,13 @@ export default class ConterConfigurator extends React.Component {
         <div className="counter-configurator-field">
           <label htmlFor="date">
             Data: 
-            <input type="date" id="date"/>
+            <input type="date" id="date" value={ this.state.date }/>
           </label>
         </div>
         <div className="counter-configurator-field">
           <label htmlFor="time">
             Hora: 
-            <input type="time" id="time"/>
+            <input type="time" id="time" value={ this.state.time }/>
           </label>
         </div>
         <div className="counter-configurator-field">
